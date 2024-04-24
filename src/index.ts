ee84fd766991ecfe6c7fa4382d12b361d214354e
Marc Littlemore <marc.littlemore@gmail.com>
2024-04-24 21:17:04 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { Clipboard, environment, LaunchType, Toast, updateCommandMetadata } from "@raycast/api";
+
+const command = async () => {
+  const now = new Date();
+
+  // Team time zones
+  const timeZones = [
+    { flag: '🇮🇱', timeZone: 'Israel' },
+    { flag: '🇬🇧', timeZone: 'Europe/London' },
+    { flag: '🇵🇱', timeZone: 'Poland' },
+    { flag: '🇨🇦', timeZone: 'Canada/Eastern' },
+    { flag: '🇺🇸', timeZone: 'America/Chicago' },
+  ];
+
+  const subtitle = timeZones.map(({ flag, timeZone }) => {
+    const time = now.toLocaleString(undefined, { timeZone, timeStyle: "short" });
+    return `${flag} - ${time}`;
+  }).join(' | ');
+
+  await updateCommandMetadata({ subtitle });
+
+  if (environment.launchType === LaunchType.UserInitiated) {
+    const toast = new Toast({
+      style: Toast.Style.Success,
+      title: "Refreshed!",
+      message: subtitle,
+    });
+    
+    toast.primaryAction = {
+      title: "Copy to Clipboard",
+      shortcut: { modifiers: ["cmd", "shift"], key: "c" },
+      onAction: () => Clipboard.copy(subtitle),
+    };
+    await toast.show();
+  }
+};
+
+export default command;
