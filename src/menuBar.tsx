4d34d4b3eae307c16d09a664b1f72dfce528e0a1
Marc Littlemore <marc.littlemore@gmail.com>
2024-04-25 09:38:36 +0100

    use standard list

--- src/menuBar.tsx
+++ src/menuBar.tsx
@@ -1,18 +1,16 @@
 import { Icon, MenuBarExtra, open } from "@raycast/api";
-import team from './team';
+import {team, getSlackUserLink} from './team';
 
-const slackTeamId = 'T02UKDKNA';
-const slackTeamDeepLink = `slack://user?team=${slackTeamId}&id=`;
-    
 export default function Command() {
   return (
     <MenuBarExtra icon='🌐'>
       <MenuBarExtra.Section title="Team">
-        {team.map((timezone) => {
-            const time = new Date().toLocaleString(undefined, { timeZone: timezone.timeZone, timeStyle: "short" });
-            const title = `${timezone.name} - ${time}`;
+        {team.map((item) => {
+            const time = new Date().toLocaleString(undefined, { timeZone: item.timeZone, timeStyle: "short" });
+            const title = `${item.name} - ${time}`;
+            const userSlackUrl = getSlackUserLink(item.slackUserId);
             return (
-                <MenuBarExtra.Item icon={timezone.flag} key={timezone.name} title={title} onAction={() => open(`${slackTeamDeepLink}${timezone.slackUserId}`)} />
+                <MenuBarExtra.Item icon={item.flag} key={item.name} title={title} onAction={() => open(userSlackUrl)} />
             );
         }
         )}
